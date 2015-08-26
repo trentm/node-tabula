426f13bcd152b432b0706be91e765bff2e6ba586
Dave Eddy <dave@daveeddy.com>
2015-08-26 13:52:56 -0400

    allow empty data with column headers

--- test/basics.test.js
+++ test/basics.test.js
@@ -20,3 +20,15 @@ test('exports', function (t) {
     t.ok(tabula.format, 'tabula.format');
     t.end();
 });
+
+test('empty table no columns', function (t) {
+    var s = tabula.format([]);
+    t.equal(s, '');
+    t.end();
+});
+
+test('empty table with columns', function (t) {
+    var s = tabula.format([], {columns: ['foo', 'bar']});
+    t.equal(s, 'FOO  BAR\n');
+    t.end();
+});
